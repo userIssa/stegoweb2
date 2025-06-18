06a78adc6c3669d6651122f0656d27e6881a6088
userIssa <toluwanimioderinde@gmail.com>
2025-06-18 22:42:29 +0100

    fix: re-add build.sh as executable~

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Download and install FFmpeg (static)
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ
-mv ffmpeg-*-static/ffmpeg /usr/local/bin/
-chmod +x /usr/local/bin/ffmpeg
